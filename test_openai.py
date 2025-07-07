8bc4a3d638466ca17062fd73a758c920b6e06766
XvKuoMing <kamasalyamov@gmail.com>
2025-07-07 09:42:41 +0300

    fix test

--- test_openai.py
+++ test_openai.py
@@ -25,7 +25,8 @@ speech_file_path = Path(__file__).parent / "speech.mp3"
 with client.audio.speech.with_streaming_response.create(
   model=os.getenv("MODEL"),
   voice=os.getenv("VOICE"),
-  input="Здравствуйте! Спасибо, что обратились в компанию Водовоз! Я ваш ассистент. Как я могу вам помочь?"
+  input="Здравствуйте! Спасибо, что обратились в компанию Водовоз! Я ваш ассистент. Как я могу вам помочь?",
+  response_format="mp3_44100_128"
 ) as response:
   response.stream_to_file(speech_file_path)
 
